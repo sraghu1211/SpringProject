f003abb39f48814c4d7fb83433ed2df59d05472c
Raghunandhan <raghu07nandhan@gmail.com>
2023-02-23 09:06:11 +0530

    Commit9

--- src/main/java/com/ragu/exception/EmployeAlreadyExistException.java
+++ src/main/java/com/ragu/exception/EmployeAlreadyExistException.java
@@ -3,13 +3,20 @@ package com.ragu.exception;
 
 
 public class EmployeAlreadyExistException extends RuntimeException{
-	private String errorMessage;
-
-	public EmployeAlreadyExistException() {
-}
+	private static final long serialVersionUID = 1L;
+	private final String errorMessage;
+	
 	public EmployeAlreadyExistException(String errorMessage) {
 		super(errorMessage);
 		this.errorMessage = errorMessage;
+	}
+	public String getErrorMessage()
+	{
+		return errorMessage;
+	}
+	public long getErrorSerialID()
+	{
+		return serialVersionUID;
 	}
 	
 }
